a27c65a2811f2614fdc418cb879dcc5212d0e7a7
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-03-02 01:53:30 +0530

    add basic header layout

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,5 +1,49 @@
+import { ModeToggle } from "./mode-toggle";
+import { Button } from "./ui/button";
+import Container from "./ui/container";
+import { Input } from "./ui/input";
+
 function Header() {
-  return <header></header>;
+  return (
+    <header className="py-4">
+      <Container className="flex items-center justify-between">
+        <a className="flex items-center gap-2" href="#">
+          <img className="size-8" src="./assets/vscode.svg" alt="vs code" />
+          <span>Visual Studio Code</span>
+        </a>
+        <nav>
+          <ul className="flex gap-4">
+            <li>
+              <a href="#">Docs</a>
+            </li>
+            <li>
+              <a href="#">Updates</a>
+            </li>
+            <li>
+              <a href="#">Blog</a>
+            </li>
+            <li>
+              <a href="#">API</a>
+            </li>
+            <li>
+              <a href="#">Extensions</a>
+            </li>
+            <li>
+              <a href="#">FAQ</a>
+            </li>
+            <li>
+              <a href="#">Github Copilot</a>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex gap-2">
+          <ModeToggle />
+          <Input type="search" placeholder="Search docs" />
+          <Button>Download</Button>
+        </div>
+      </Container>
+    </header>
+  );
 }
 
 export default Header;
