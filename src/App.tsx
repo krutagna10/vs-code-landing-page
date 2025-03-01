6775a6d20630bd3e9a99846959f674625a94fd34
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-03-01 23:08:39 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { ThemeProvider } from "@/components/theme-provider"
+import { ThemeProvider } from "@/components/theme-provider";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <main>
-        <h1>React ShadCN Template</h1>
+        <h1>VS Code Landing Page</h1>
+        <ModeToggle />
       </main>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
