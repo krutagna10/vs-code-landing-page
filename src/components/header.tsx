a1134c2508ac1c77a83365cb1eba29525500ec80
Krutagna Patel <kpatel.kp3101@gmail.com>
2025-03-02 02:22:49 +0530

    add search input

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,45 +1,70 @@
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
 import Container from "./ui/container";
-import { Input } from "./ui/input";
+import { Search } from "lucide-react";
 
 function Header() {
   return (
     <header className="py-4">
       <Container className="flex items-center justify-between">
         <a className="flex items-center gap-2" href="#">
-          <img className="size-8" src="./assets/vscode.svg" alt="vs code" />
-          <span>Visual Studio Code</span>
+          <img className="size-6" src="./assets/vscode.svg" alt="vs code" />
+          <span className="text-xl font-bold text-gray-300">
+            Visual Studio Code
+          </span>
         </a>
         <nav>
           <ul className="flex gap-4">
             <li>
-              <a href="#">Docs</a>
+              <a className="text-gray-300 hover:text-white" href="#">
+                Docs
+              </a>
             </li>
             <li>
-              <a href="#">Updates</a>
+              <a className="text-gray-300 hover:text-white" href="#">
+                Updates
+              </a>
             </li>
             <li>
-              <a href="#">Blog</a>
+              <a className="text-gray-300 hover:text-white" href="#">
+                Blog
+              </a>
             </li>
             <li>
-              <a href="#">API</a>
+              <a className="text-gray-300 hover:text-white" href="#">
+                API
+              </a>
             </li>
             <li>
-              <a href="#">Extensions</a>
+              <a className="text-gray-300 hover:text-white" href="#">
+                Extensions
+              </a>
             </li>
             <li>
-              <a href="#">FAQ</a>
+              <a className="text-gray-300 hover:text-white" href="#">
+                FAQ
+              </a>
             </li>
             <li>
-              <a href="#">Github Copilot</a>
+              <a className="text-gray-300 hover:text-white" href="#">
+                Github Copilot
+              </a>
             </li>
           </ul>
         </nav>
         <div className="flex gap-2">
           <ModeToggle />
-          <Input type="search" placeholder="Search docs" />
-          <Button>Download</Button>
+          <label className="flex items-center gap-2 px-2 outline-1 outline-gray-700 has-focus:outline-sky-700">
+            <Search size="18" />
+            <input
+              type="search"
+              placeholder="Search docs"
+              className="focus:outline-0"
+            />
+          </label>
+          <Button className="cursor-pointer bg-sky-500 text-white hover:bg-sky-600 active:bg-sky-700">
+            Download
+          </Button>
         </div>
       </Container>
     </header>
